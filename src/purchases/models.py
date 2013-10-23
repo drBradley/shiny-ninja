8fc6ad07e6bb119d7966f86dc8b1320f5076182a
Karol Marcjan <karol.marcjan@gmail.com>
2013-10-23 12:26:35 +0200

    Add purchases.models.Purchase

--- src/purchases/models.py
+++ src/purchases/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+from products.models import Price
+
+
+class Purchase(models.Model):
+
+    number = models.PositiveIntegerField(default=1)
+    product_price = models.ForeignKey(Price)
+    payer = models.ForeignKey(User)
+    date = models.DateField(
+        default=timezone.now)
+
+    def __unicode__(self):
+
+        return "%s bought %d %s for %s on %s" % (
+            self.payer,
+            self.number,
+            self.product_price.product.name,
+            self.product_price.value,
+            self.date)
